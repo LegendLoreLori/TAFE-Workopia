2b2a2b68faa9f101b989712ddd3efddb800b0d1b
Lori <lorenzomandolfino@gmail.com>
2024-02-26 13:39:16 +0800

    complete ch01

--- ch01-intro/ch01.php
+++ ch01-intro/ch01.php
@@ -1,4 +1,13 @@
 <?php
+$title = "Introduction to PHP";
+$author = "<p>By: John Doe</p>";
+$body = "<p>
+        PHP (Hypertext Preprocessor) is a widely used server-side scripting
+        language that has revolutionized web development. With its simplicity,
+        flexibility, and vast community support, PHP has become the backbone of
+        countless dynamic websites and web applications.
+    </p>";
+$page_title = "Brad's PHP Blog | $title"
 
 ?>
 <!DOCTYPE html>
@@ -6,18 +15,13 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brad's PHP Blog | Introduction to PHP</title>
+    <title><?= $page_title ?></title>
 </head>
 <body>
 <main>
-    <h1>Introduction to PHP</h1>
-    <p>By: John Doe</p>
-    <p>
-        PHP (Hypertext Preprocessor) is a widely used server-side scripting
-        language that has revolutionized web development. With its simplicity,
-        flexibility, and vast community support, PHP has become the backbone of
-        countless dynamic websites and web applications.
-    </p>
+    <h1><?= $title ?></h1>
+    <?= $author ?>
+    <?= $body ?>
 </main>
 </body>
 </html>

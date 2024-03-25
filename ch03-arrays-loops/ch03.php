47fea1fe120cd6c7f8d55f37361278a838cb8523
Lori <lorenzomandolfino@gmail.com>
2024-03-25 12:27:00 +0800

    complete challenge 1

--- ch03-arrays-loops/ch03.php
+++ ch03-arrays-loops/ch03.php
@@ -1,1 +1,9 @@
 <?php
+// challenge 1
+$numbers = [1, 2, 3, 4, 5];
+$amount = count($numbers);
+$sum = 0;
+foreach ($numbers as $num){
+    $sum += $num;
+}
+echo "The sum of $amount numbers is $sum";

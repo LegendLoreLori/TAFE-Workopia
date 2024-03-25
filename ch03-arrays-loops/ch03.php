c26363962fe594a2835e9aa28d8e5769f0ad109b
Lori <lorenzomandolfino@gmail.com>
2024-03-25 12:51:56 +0800

    complete challenge 2

--- ch03-arrays-loops/ch03.php
+++ ch03-arrays-loops/ch03.php
@@ -6,4 +6,20 @@ $sum = 0;
 foreach ($numbers as $num){
     $sum += $num;
 }
-echo "The sum of $amount numbers is $sum";
+echo "The sum of $amount numbers is $sum<br>";
+
+// challenge 2
+$colours = ["red", "blue", "green", "yellow"];
+$colours = array_reverse($colours);
+echo "<pre>" . print_r($colours, true) . "</pre>";
+
+$colours = array_merge($colours, ["purple", "orange"]);
+echo "<pre>" . print_r($colours, true) . "</pre>";
+
+array_splice($colours,1,0, "pink");
+echo "<pre>" . print_r($colours, true) . "</pre>";
+
+array_pop($colours);
+echo "<pre>" . print_r($colours, true) . "</pre>";
+
+

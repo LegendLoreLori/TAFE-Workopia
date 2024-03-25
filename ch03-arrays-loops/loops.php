413e76373a35519be9158cf6fe61642e915ee5c2
Lori <lorenzomandolfino@gmail.com>
2024-03-25 14:31:04 +0800

    complete challenge 2

--- ch03-arrays-loops/loops.php
+++ ch03-arrays-loops/loops.php
@@ -6,3 +6,11 @@ for ($i = 1; $i <= 12; $i++) {
     }
 }
 
+// Challenge 2
+$numbers = [1, 2, 3, 4, 5];
+$amount = count($numbers);
+$sum = 0;
+foreach ($numbers as $num){
+    $sum += $num;
+}
+echo "The sum of $amount numbers is $sum<br>";

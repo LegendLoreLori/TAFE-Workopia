9ec6f10706da42e3555eb7472e37967ca8721cb6
Lori <lorenzomandolfino@gmail.com>
2024-03-27 12:48:11 +0800

    complete fizzbuzz challenge

--- ch04-conditionals-etc/fizzbuzz-challenge.php
+++ ch04-conditionals-etc/fizzbuzz-challenge.php
@@ -1,1 +1,15 @@
 <?php
+$fizz = 3;
+$buzz = 5;
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % $fizz == 0 && $i % $buzz == 0) {
+        echo "$i fizzbuzz<br>";
+    } else if ($i % $fizz == 0) {
+        echo "$i fizz<br>";
+    } else if ($i % $buzz == 0) {
+        echo "$i buzz<br>";
+    } else {
+        echo "$i<br>";
+    }
+}

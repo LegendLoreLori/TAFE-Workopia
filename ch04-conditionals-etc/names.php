829c6293668df634003225902de9aba0f06eb720
Lori <lorenzomandolfino@gmail.com>
2024-03-27 13:05:48 +0800

    complete names challenge

--- ch04-conditionals-etc/names.php
+++ ch04-conditionals-etc/names.php
@@ -1,2 +1,8 @@
 <?php
 $names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Elanor', 'Anna', 'Freddie', 'Adam'];
+foreach ($names as $name) {
+    if (ucwords($name)[0] == 'A') {
+        continue;
+    }
+    echo strtolower(strrev($name)) . "<br>";
+}

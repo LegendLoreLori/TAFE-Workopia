feb545783bf93df956503fc63eef1b4b23cd843e
Lori <lorenzomandolfino@gmail.com>
2024-03-29 13:00:52 +0800

    complete challenge 1 harder (worse ?)

--- ch05-functions/more-function.php
+++ ch05-functions/more-function.php
@@ -1,7 +1,16 @@
 <?php
-
 // Easy implementation
 $convertFahrenheit = fn ($tempF) : string =>
     $tempF . "F = " . 5/9 * ($tempF - 32) . "C";
 
 echo $convertFahrenheit(54);
+
+// Harder implementation
+$temperature = 32;
+
+$convertFahrenheit2 = function ($tempF) use (&$temperature) {
+  return $tempF . "F = " . 5/9 * ($tempF - $temperature) . "C";
+};
+
+echo "<br>";
+echo $convertFahrenheit2(54);

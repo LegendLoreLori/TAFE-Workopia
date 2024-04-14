2b08b4e6155c1bbd98695c60c26af4f49a1ea24b
Lori <lorenzomandolfino@gmail.com>
2024-04-14 10:19:02 +0800

    add loadView and loadPartial helper functions

--- Workopia/views/home.php
+++ Workopia/views/home.php
@@ -1,8 +1,8 @@
 <?php
-require basePath('views/partials/head.php');
-require basePath('views/partials/navbar.php');
-require basePath('views/partials/showcase-search.php');
-require basePath('views/partials/top-banner.php');
+loadPartial('header');
+loadPartial('navbar');
+loadPartial('showcase-search');
+loadPartial('top-banner');
 ?>
 
     <!-- Job Listings -->
@@ -194,6 +194,6 @@ We're looking for a Data Scientist to analyze complex data and
           Show All Jobs
         </a>
       </section>
-<?php require basePath('views/partials/bottom-banner.php'); ?>
+<?php loadPartial( 'bottom-banner'); ?>
 
-<?php require basePath('views/partials/footer.php'); ?>
+<?php loadPartial( 'footer'); ?>

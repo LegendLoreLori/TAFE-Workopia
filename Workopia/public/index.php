ac25b90b530bc1ae318a6e8f6bc038c01626214c
Lori <lorenzomandolfino@gmail.com>
2024-04-15 12:28:36 +0800

    add .htaccess file, add routes array

--- Workopia/public/index.php
+++ Workopia/public/index.php
@@ -1,3 +1,11 @@
 <?php
 require '../helpers.php';
-loadView('home');
+
+//loadView('home');
+$routes =
+[
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];

ff4c3d72f566fa4943ceb5db59880a2d12a5bc65
Lori <lorenzomandolfino@gmail.com>
2024-04-17 11:05:14 +0800

    controllers dir, add basic routing logic to index.php

--- Workopia/public/index.php
+++ Workopia/public/index.php
@@ -9,3 +9,12 @@ $routes =
     '/listings/create' => 'controllers/listings/create.php',
     '404' => 'controllers/error/404.php'
 ];
+
+$uri = $_SERVER['REQUEST_URI'];
+// Run SaaS-LM-Challenges.test for now
+
+if(array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    require basePath($routes['404']);
+}

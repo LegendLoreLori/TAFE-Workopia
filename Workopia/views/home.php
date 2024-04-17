e836f88c4dd832f65e87ea31d4610f6df5834c3a
Lori <lorenzomandolfino@gmail.com>
2024-04-17 11:11:33 +0800

    add controllers dir, add 404, create, index controllers

--- Workopia/views/home.php
+++ Workopia/views/home.php
@@ -189,7 +189,7 @@ We're looking for a Data Scientist to analyze complex data and
             </div>
           </div>
         </div>
-        <a href="listings.html" class="block text-xl text-center">
+        <a href="/listings" class="block text-xl text-center">
           <i class="fa fa-arrow-alt-circle-right"></i>
           Show All Jobs
         </a>

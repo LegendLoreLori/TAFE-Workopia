08098f5c185aa9b65bb3a0c7390189224ef0c2b4
Lori <lorenzomandolfino@gmail.com>
2024-04-27 18:57:24 +0800

    add update method to listing controller

--- Workopia/App/controllers/ListingController.php
+++ Workopia/App/controllers/ListingController.php
@@ -139,7 +139,7 @@ class ListingController
 
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-        if(!$listing) {
+        if (!$listing) {
             ErrorController::notFound('Listing not found');
             return;
         }
@@ -177,5 +177,66 @@ class ListingController
         loadView('/listings/edit', [
             'listing' => $listing
         ]);
+    }
+
+    /**
+     * Update a listing
+     *
+     * @param array $params
+     * @return void
+     * @throws \Exception
+     */
+    public function update(array $params): void
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company',
+                          'address', 'city', 'state', 'phone', 'email',
+                          'requirements', 'benefits'];
+        $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+        $updateValues = array_map('sanitise', $updateValues);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city',
+                           'state'];
+
+        $errors = [];
+        foreach ($requiredFields as $field) {
+            if(empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field . ' is required');
+            }
+        }
+
+        if(!empty($errors)) {
+            loadView('listings/edit', [
+                'listing' => $listing,
+                'errors' => $errors
+            ]);
+        } else {
+            // submit to database
+            $updateFields = [];
+            foreach (array_keys($updateValues) as $field) {
+                $updateFields[] = "$field = :$field";
+            }
+            $updateFields = implode(', ', $updateFields);
+            $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+            $updateValues['id'] = $id;
+
+            $this->db->query($updateQuery, $updateValues);
+
+            $_SESSION['success_message'] = 'Listing updated.';
+            redirect('/listings/' . $id);
+        }
     }
 }
--- Workopia/App/views/listings/show.php
+++ Workopia/App/views/listings/show.php
@@ -7,6 +7,7 @@ loadPartial('top-banner');
 
   <section class="container mx-auto p-4 mt-4">
     <div class="rounded-lg shadow-md bg-white p-3">
+        <?php loadPartial('message') ?>
       <div class="flex justify-between items-center">
         <a class="block p-4 text-blue-700" href="/listings">
           <i class="fa fa-arrow-alt-circle-left"></i>
@@ -18,7 +19,7 @@ loadPartial('top-banner');
           <!-- Delete Form -->
           <form method="POST">
             <input type="hidden" name="_method"
-            value="DELETE">
+                   value="DELETE">
             <button type="submit"
                     class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">
               Delete
